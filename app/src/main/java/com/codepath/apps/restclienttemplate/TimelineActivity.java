fe3ee72ac6f11fe804c413a7aac46df324779400
Fianko Buckle <kingfi@fb.com>
2020-07-02 19:03:11 -0700

    Added persistence with sqlLite

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -14,6 +15,9 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.apps.restclienttemplate.models.TweetDao;
+import com.codepath.apps.restclienttemplate.models.TweetWithUser;
+import com.codepath.apps.restclienttemplate.models.User;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
 
@@ -32,6 +36,7 @@ public class TimelineActivity extends AppCompatActivity {
 
     public static final int REQUEST_CODE = 20;
 
+    TweetDao tweetDao;
     RestClient client;
     RecyclerView rvTweets;
     List<Tweet> tweets;
@@ -46,6 +51,8 @@ public class TimelineActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate!");
 
         client = RestApplication.getRestClient(this);
+        tweetDao = ((RestApplication) getApplicationContext()).getMyDatabase().tweetDao();
+
 
         swipeContainer = findViewById(R.id.swipeContainer);
 
@@ -86,6 +93,17 @@ public class TimelineActivity extends AppCompatActivity {
 
         // Adds the scroll listener to RecyclerView
         rvTweets.addOnScrollListener(scrollListener);
+
+        // Query for existing tweets in the DB
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                 List<TweetWithUser> tweetWithUsers = tweetDao.recentItems();
+                 List<Tweet> tweetsFromDB = TweetWithUser.getTweetList(tweetWithUsers);
+                 adapter.clear();
+                 adapter.addAll(tweetsFromDB);
+            }
+        });
 
         populateHomeTimeline();
 
@@ -158,10 +176,22 @@ public class TimelineActivity extends AppCompatActivity {
                 Log.i(TAG, "onSuccess!");
                 JSONArray jsonArray = json.jsonArray;
                 try{
+                    final List<Tweet> tweetsFromNetwork = Tweet.fromJsonArray(jsonArray);
                     adapter.clear();
                     adapter.addAll(Tweet.fromJsonArray(jsonArray));
                     // Signal refresh has finished
                     swipeContainer.setRefreshing(false);
+
+                    AsyncTask.execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            //insert users first
+                            List<User> usersFromNetwork = User.fromJsonTweetArray(tweetsFromNetwork);
+                            tweetDao.insertModel(usersFromNetwork.toArray(new User[0]));
+                            //insert tweets next
+                            tweetDao.insertModel(tweetsFromNetwork.toArray(new Tweet[0]));
+                        }
+                    });
                 } catch (JSONException e){
                     Log.e(TAG, "Json exception");
                 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -2,6 +2,12 @@ package com.codepath.apps.restclienttemplate.models;
 
 import android.util.Log;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,13 +17,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Parcel
+@Entity(foreignKeys = @ForeignKey(entity=User.class, parentColumns="id", childColumns="userId"))
 public class Tweet {
 
-    public String body;
-    public String createdAt;
-    public User user;
+    @ColumnInfo
+    @PrimaryKey
     public long id;
+
+    @ColumnInfo
+    public String body;
+
+    @ColumnInfo
+    public String createdAt;
+
+    @Ignore
     public ArrayList<String> urls;
+
+    @Ignore
+    public User user;
+
+    @ColumnInfo
+    public long userId;
+
 
     // empty constructor requited for Parcel
     public Tweet() {}
@@ -27,8 +48,10 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.id = jsonObject.getLong("id");
-        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        User user = User.fromJson((jsonObject.getJSONObject("user")));
+        tweet.user = user;
         tweet.urls = new ArrayList<String>();
+        tweet.userId = user.id;
         try{
             JSONObject entities = jsonObject.getJSONObject("extended_entities");
             JSONArray media = entities.getJSONArray("media");
